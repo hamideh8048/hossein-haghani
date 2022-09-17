eb69416ed9aa7fc37b42a2fe06d3ca7e579f9083
hamideh8048 <hamideh.esm@gmail.com>
2022-09-18 01:12:59 +0430

    add modal

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,36 +1,88 @@
 import React from "react";
 import { productList } from "../imagejsonlist";
-
+import Modal from "react-modal";
 import Pagination from "../Pagination";
+import "../styles/style.css";
+
 
 class ProductList extends React.Component {
+ 
+
   constructor(props) {
     super(props);
+
     this.state = {
-      productDataList: []
+
+      productDataList: [],
+      modalIsOpen: false,
+      name:'',
+      isOpen: false
     };
   }
 
+
   onChangePage = pageOfItems => {
     this.setState({
-      productDataList: pageOfItems
+      productDataList: pageOfItems,
+      modalIsOpen: false,
+      item:[]
     });
   };
-
+  
+  // openModal = () => {
+  //   this.setState({ modalIsOpen: true });
+  //   this.setState({ name: 'a'});
+  // };
+  openModal(someValue) { // called repeatedly
+    this.setState({ modalIsOpen: true });
+    this.setState({ name: someValue});
+  }
+  closeModal = () => {
+    this.setState({ modalIsOpen: false ,
+    });
+  };
   render() {
     const { productDataList } = this.state;
 
+    const modalStyle = {
+      overlay: {
+        position: "fixed",
+        top: 0,
+        left: 0,
+        backgroundColor: "rgba(0,0,0,0.85)"
+      },
+      content: {
+        position: "absolute",
+        top: "5rem",
+        left: "5rem",
+        right: "5rem",
+        bottom: "5rem",
+        backgroundColor: "paleturquoise",
+        borderRadius: "1rem",
+        padding: "1.5rem"
+      }
+    };
+    
+    Modal.setAppElement("#root");
     return (
       <div>
-  
-    
-          {productDataList.map(item => (
-    
-  <div>
+      
+        {productDataList.map(item => (
+        <div>
         <img  src={item.url} alt="caligraphy"  class="center"></img>
-<br></br>
+    
+        <a  className="read-more-link" onClick={()=>this.openModal(item.name)}><h3>{'اطلاعات بیشتر'}</h3></a>
+    <Modal   isOpen={this.state.modalIsOpen}  onRequestClose={this.closeModal} style={modalStyle}  overlayClassName="myoverlay"
+        closeTimeoutMS={500}>
+   
+
+      <button onClick={this.closeModal}>close</button>
+      <div>{this.state.name}</div>
+    </Modal>
+
 </div>
-      ))}
+      )
+      )}
 
     <div class="center" >
         <Pagination
